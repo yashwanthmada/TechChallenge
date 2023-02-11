333962a2049a643c6309a6d3f29dbc225231af72
yashwanthmada <53607981+yashwanthmada@users.noreply.github.com>
2023-02-11 22:45:59 +0530

    Add files via upload

---
+++ GetInstanceDetailsAndKeysInJSON.ps1
@@ -0,0 +1,20 @@
+﻿Connect-AzAccount
+Set-AzContext -Subscription "<SubscriptionName>"
+
+## Retrieving a VM instance metdata using its name
+$instanceDetails = Get-AzVM -ResourceGroupName "<ResourceGroupName>" -Name "<VMName>"
+
+## Get instance details in JSON format using ConvertTo-JSON cmdlet
+$instanceDetailsinJson = $instanceDetails | ConvertTo-Json
+
+## Query/Retrive any Key and Value by Converting the JSON to PSCustomObject
+$data = $instanceDetailsinJson | ConvertFrom-Json
+
+## Example: Retreive Values for Specific required Keys - "ResourceGroupName,Type,VmId" of instance
+$data.ResourceGroupName
+$data.Type
+$data.VmId
+
+
+
+
---
+++ ObjectkeyValue.ps1
@@ -0,0 +1,16 @@
+﻿function get-value {
+    param (
+        [parameter(Mandatory = $true)]
+        [String]$object,
+        [parameter(Mandatory = $true)]
+        [String]$Key,
+        [String]$value 
+    ) 
+    $SimplifiedObject = $object.Split('{":}') | Where-Object {$_ -ne ""}
+    $simplifiedKey = $key.Split('/') | Where-Object {$_ -ne ""}
+    $value = $SimplifiedObject | Where-Object {$_ -notin $simplifiedKey}
+    Write-Host "value is $($value)"
+}
+
+get-value -object '{"a":{"b":{"c":"d"}}}' -key 'a/b/c'
+
